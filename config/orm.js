9e3318cfc32e38909af8b433f4536f6fd6a7c564
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-07-26 22:17:34 -0700

    constructs handlebars and adds helper function to ORM

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,29 @@
 var connection = require('../config/connection.js');
 
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function for SQL syntax.
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
 var orm = {
 	selectAll: function(tableInput, cb) {
 	    var queryString = "SELECT * FROM " + tableInput + ";";
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,1 +1,20 @@
 var express = require('express');
+
+var router = express.Router();
+
+//Import function model
+var burger = require('../models/burger.js');
+
+//Create the app routes
+router.get('/', function(req, res){
+	burger.selectAll(function(data){
+		var allBurgsObj = {
+			burgers: data
+		};
+		console.log('allBurgsObj: ', allBurgsObj);
+		res.render(allBurgsObj);
+	});
+});
+
+// Export routes for server.js to use.
+module.exports = router;
