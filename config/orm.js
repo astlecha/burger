2460d8e23a69f08b4dc261339cad73bf4afccaef
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-07-26 21:28:35 -0700

    updates orm selectAll functino

--- config/orm.js
+++ config/orm.js
@@ -3,9 +3,9 @@ var connection = require('../config/connection.js');
 
 
 var orm = {
-	selectAll: function(tableInput, colToSearch, valOfCol) {
-	    var queryString = 'SELECT * FROM ?? WHERE ?? = ?';
-	    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+	selectAll: function(tableInput) {
+	    var queryString = 'SELECT * FROM ??';
+	    connection.query(queryString, tableInput, function(err, result) {
 	      console.log(result);
 	    });
 	},
@@ -18,9 +18,9 @@ var orm = {
 	      cb(result);
 	    });
 	},
-	updateOne: function(tableInput, vals, condition, cb){
+	updateOne: function(tableInput, objColVals, condition, cb){
 		var queryString = 'UPDATE ?? SET ? WHERE ??';
-		connection.query(queryString, [tableInput, vals, condition], function(err, result) {
+		connection.query(queryString, [tableInput, objColVals, condition], function(err, result) {
 			if (err) {
 	        throw err;
 	      }
