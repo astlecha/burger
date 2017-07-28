cbedf31483ba27c5df055fdc467341974171e632
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-07-27 21:39:26 -0700

    updates heroku schema

--- config/connection.js
+++ config/connection.js
@@ -6,13 +6,12 @@ if(process.env.JAWSDB_URL){
 }
 else {
 	connection = mysql.createConnection({
-	port: 3306,
 	host: 'localhost',
 	user: 'root',
 	password: '',
 	database: 'burgers_db'
-}
-});
+	})
+};
 
 connection.connect(function(err){
 	if(err){
